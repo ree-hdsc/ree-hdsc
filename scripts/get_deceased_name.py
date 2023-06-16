d26bc4dcbbcdf7bee654325e93edec6cbca069cf
Erik Tjong Kim Sang <erikt@xs4all.nl>
2023-06-16 20:51:42 +0200

    extracting names from infofields

--- scripts/get_deceased_name.py
+++ scripts/get_deceased_name.py
@@ -1,4 +1,4 @@
-import re
+import regex
 import transformers
 from . import utils
 
@@ -18,10 +18,10 @@ run_bert_pipeline = transformers.pipeline(task='ner', model='wietsedv/bert-base-
 def show_names(entities):
     name = ""
     for part in entities:
-        if re.search("^B", part["entity"]) and name != "":
+        if regex.search("^B", part["entity"]) and name != "":
             print(name)
             name = ""
-        if re.search("(GPE|PERSON)$", part["entity"]):
+        if regex.search("(GPE|PERSON)$", part["entity"]):
             if name != "":
                 name += " "
             name += part["word"]
@@ -34,7 +34,8 @@ def expand_entities(entities_in, text):
     for entity_in in entities_in:
         entity_out = entity_in.copy()
         while (entity_out["end"] < len(text) and
-               (re.search("\w", text[entity_out["end"]]) or re.search("[.,-]", text[entity_out["end"]]))):
+               (regex.search("\w", text[entity_out["end"]]) or 
+                regex.search("[.,-]", text[entity_out["end"]]))):
             entity_out["word"] += text[entity_out['end']]
             entity_out["end"] += 1
         entities_out.append(entity_out)
@@ -52,10 +53,10 @@ def combine_entities(entities_in):
         entity_out = entity_in.copy()
         if len(entities_out) == 0:
             entities_out.append(entity_out)
-        elif re.search("^I-", entity_out["entity"]):
+        elif regex.search("^I-", entity_out["entity"]):
             expand_last_entity(entities_out, entity_out)
         else:
-            entity_out["entity"] = re.sub("^[BIE]-", "B-", entity_out["entity"])
+            entity_out["entity"] = regex.sub("^[BIE]-", "B-", entity_out["entity"])
             if entity_out["start"] < entities_out[-1]["start"]:
                 print("error: entities are not sorted by position!")
             elif entity_out["start"] <= entities_out[-1]["end"] + 1 and entity_out["entity"] == entities_out[-1]["entity"]:
@@ -75,9 +76,9 @@ def process_and_render_texts(texts):
 
 
 def cleanup(text_in):
-    text_out = re.sub("\s+", " ", text_in)
-    text_out = re.sub("- ", "", text_out)
-    return re.sub("[,.]", "", text_out.lower())
+    text_out = regex.sub("\s+", " ", text_in)
+    text_out = regex.sub("- ", "", text_out)
+    return regex.sub("[,.]", "", text_out.lower())
 
 
 def compare_names(results, metadata):
@@ -86,14 +87,14 @@ def compare_names(results, metadata):
     if "first_names" not in metadata or "last_name" not in metadata:
         return(False)
     guessed_name = results[0][0]
-    if re.search(".,.", guessed_name):
-        guessed_name = re.sub("^[^,]+, *(\S.*)$", "\\1", results[0][0]) + " " + re.sub("^([^,]+),.*$", "\\1", results[0][0])
+    if regex.search(".,.", guessed_name):
+        guessed_name = regex.sub("^[^,]+, *(\S.*)$", "\\1", results[0][0]) + " " + regex.sub("^([^,]+),.*$", "\\1", results[0][0])
     annotated_name = " ".join([ metadata["first_names"], metadata["last_name"]])
     return cleanup(guessed_name) == cleanup(annotated_name)
 
 
 def evaluate_deceased_names(results, nbr_of_names_found, nbr_of_stillborns_found, metadata):
-    if len(results[0]) != 0 and re.search("\w", results[0][0]):
+    if len(results[0]) != 0 and regex.search("\w", results[0][0]):
         nbr_of_names_found += 1
     if results[1] > 0:
         nbr_of_stillborns_found += 1
@@ -135,7 +136,37 @@ def get_name_of_deceased_from_entities(text, entities):
     return deceased
 
 
-def get_name_of_deceased_from_text(text):
+def get_next_token(entity, text):
+    next_token = ""
+    start_id = entity["end"]
+    while start_id < len(text) and regex.search("\s", text[start_id]):
+        start_id += 1
+    end_id = start_id + 1
+    while end_id < len(text) and regex.search("\S", text[end_id]):
+        end_id += 1
+    if start_id < len(text):
+        next_token = text[start_id: end_id]
+    return next_token, end_id
+
+
+def combine_capitalized_words(entities, text):
+    for i in range(0, len(entities)):
+        if regex.search("(PERSON|GPE)", entities[i]["entity"]):
+            next_token, end_id = get_next_token(entities[i], text)
+            while next_token != "" and regex.search("^[A-Z]\\b", next_token) and not regex.search(",", text[entities[i]["start"]: entities[i]["end"]]):
+                entities[i]["end"] = end_id
+                entities[i]["word"] = text[entities[i]["start"]: entities[i]["end"]]
+                next_token, end_id = get_next_token(entities[i], text)
+    return entities
+
+
+def get_entities_from_text(text):
     entities = run_bert_pipeline(text)
     entities = combine_entities(expand_entities(entities, text))
+    entities = combine_capitalized_words(entities, text)
+    return entities
+
+
+def get_name_of_deceased_from_text(text):
+    entities = get_entities_from_text(text)
     return get_name_of_deceased_from_entities(text, entities)
